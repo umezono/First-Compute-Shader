d3dff8abfcba8ea20fe376b296cb170bc464478c
umezono_win <takashi.umezono@gmail.com>
2019-06-17 08:53:27 +0900

    まとめてコミット、カメラの設定をDeferredに変更

---
+++ Assets/FirstComputeShader/Script/InstancingMesh.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Rendering;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+public class InstancingMesh : MonoBehaviour
+{
+    #region Propaty
+    [SerializeField]
+    private int _numInstance = 100;
+
+    public Mesh _mesh;
+
+    public Material _material;
+
+    [SerializeField]
+    private int[] _shaderPasses = new int[] { 0 };
+
+    [SerializeField]
+    private MeshTopology _topology = MeshTopology.Triangles;
+
+    [SerializeField]
+    private CameraEvent _commandAt = CameraEvent.AfterGBuffer;
+
+    [SerializeField]
+    private float _initPosRange = 10f;
+
+    public ComputeShader _updater;
+
+    private ComputeBuffer _meshIndicesBuffer;
+    private ComputeBuffer _meshVertexDataBuffer;
+    private ComputeBuffer _instanceDataBuffer;
+
+    private CommandBuffer _command;
+
+    private List<Camera> _targetCamList = new List<Camera>();
+    #endregion
+
+    #region Struct
+    struct VertexData
+    {
+        public Vector3 vert;
+        public Vector3 normal;
+        public Vector2 uv;
+        public Vector4 tangent;
+    }
+
+    struct InstanceData
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public float scale;
+    }
+    #endregion
+
+    #region MonoBehaviour Functions
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetData();
+    }
+
+    void OnDestroy()
+    {
+        ReleaseData();      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateData();
+    }
+
+    void OnRenderObject()
+    {
+        if (_targetCamList.Contains(Camera.current)) return;
+
+        Camera.current.AddCommandBuffer(_commandAt, _command);
+        _targetCamList.Add(Camera.current);
+    }
+    #endregion
+
+    #region Private Functions
+    void SetData()
+    {
+        var indices = _mesh.GetIndices(0);
+
+        var vertexDataArray = Enumerable.Range(0, _mesh.vertexCount).Select(b => {
+            return new VertexData()
+            {
+                vert = _mesh.vertices[b],
+                normal = _mesh.normals[b],
+                uv = _mesh.uv[b],
+                tangent = _mesh.tangents[b]
+            };
+        }).ToArray();
+
+        var instanceDataArray = Enumerable.Range(0, _numInstance).Select(b => {
+            return new InstanceData()
+            {
+                position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * _initPosRange,
+                rotation = Random.rotation,
+                scale = Random.value
+            };
+        }).ToArray();
+
+        _meshIndicesBuffer = new ComputeBuffer(indices.Length, Marshal.SizeOf(typeof(int)));
+        _meshVertexDataBuffer = new ComputeBuffer(vertexDataArray.Length, Marshal.SizeOf(typeof(VertexData)));
+        _instanceDataBuffer = new ComputeBuffer(instanceDataArray.Length, Marshal.SizeOf(typeof(InstanceData)));
+
+        _meshIndicesBuffer.SetData(indices);
+        _meshVertexDataBuffer.SetData(vertexDataArray);
+        _instanceDataBuffer.SetData(instanceDataArray);
+
+        _material.SetBuffer("_Indices", _meshIndicesBuffer);
+        _material.SetBuffer("_vData", _meshVertexDataBuffer);
+        _material.SetBuffer("_iData", _instanceDataBuffer);
+
+        _command = new CommandBuffer();
+        _command.name = name + ".instancingMesh";
+
+        foreach (var shaderPass in _shaderPasses)
+            _command.DrawProcedural(Matrix4x4.identity, _material, shaderPass, _topology, indices.Length, _numInstance);
+    }
+
+    void ReleaseData()
+    {
+        new[] { _meshIndicesBuffer, _meshVertexDataBuffer, _instanceDataBuffer }.Where(b => b != null).ToList().ForEach(b =>
+        {
+            b.Release();
+            b = null;
+        });
+    }
+
+    void UpdateData()
+    {
+        var kernel = _updater.FindKernel("CSMain");
+        _updater.SetBuffer(kernel, "_iData", _instanceDataBuffer);
+        _updater.Dispatch(kernel, _numInstance / 1024 + 1, 1, 1);
+    }
+    #endregion
+}
---
+++ Assets/FirstComputeShader/Shader/Visualize.shader
@@ -0,0 +1,137 @@
+﻿Shader "Unlit/Visualize"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        // LOD 100
+
+        Pass
+        {
+			Name "DEFERRED"
+
+			Tags{"LightMode" = "Deferred"}
+
+			Stencil{
+				Comp Always
+				Pass Replace
+				Ref 128
+			}
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            //#pragma multi_compile_fog
+			#pragma target 5.0
+
+            #include "UnityCG.cginc"
+			#include "../Cginc/Quaternion.cginc"
+
+			struct VertexData {
+				float3 vertex;
+				float3 normal;
+				float2 uv;
+				float4 tangent;
+			};
+			struct InstanceData {
+				float3 position;
+				float4 rotation;
+				float scale;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv		: TEXCOORD0;
+				float3 normal	: TEXCOORD1;
+				float4 tangent	: TEXCOORD2;
+			};
+			struct pOut {
+				half4 diffuse           : SV_Target0; // RT0: diffuse color (rgb), occlusion (a)
+				half4 spec_smoothness   : SV_Target1; // RT1: spec color (rgb), smoothness (a)
+				half4 normal            : SV_Target2; // RT2: normal (rgb), --unused, very low precision-- (a) 
+				half4 emission          : SV_Target3; // RT3: emission (rgb), --unused-- (a)
+			};
+
+			StructuredBuffer<uint> _Indices;
+			StructuredBuffer<VertexData> _vData;
+			StructuredBuffer<InstanceData> _iData;
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _Offset;
+
+			v2f vert(uint vid : SV_VertexID, uint iid : SV_InstanceID)
+			{
+				uint idx = _Indices[vid];
+				float4 pos = float4(_vData[idx].vertex, 1.0);
+				float2 uv = _vData[idx].uv;
+				float3 normal = _vData[idx].normal;
+				float4 tangent = _vData[idx].tangent;
+				float4 rotation = _iData[iid].rotation;
+
+				pos.xyz *= _iData[iid].scale;
+				pos.xyz = rotateWithQuaternion(pos.xyz, rotation);
+				pos.xyz += _iData[iid].position;
+
+				normal = rotateWithQuaternion(normal, rotation);
+
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_VP, pos);
+				o.uv = uv;
+				o.normal = normal;
+				o.tangent = tangent;
+				return o;
+			}
+
+			pOut frag(v2f i)
+			{
+				pOut o;
+				o.diffuse = tex2D(_MainTex, i.uv);
+				o.spec_smoothness = 0.2;
+				o.normal = float4(i.normal*0.5 + 0.5, 0.0);
+				o.emission = 0.0;
+				return o;
+			}
+
+            //struct appdata
+            //{
+            //    float4 vertex : POSITION;
+            //    float2 uv : TEXCOORD0;
+            //};
+
+            //struct v2f
+            //{
+            //    float2 uv : TEXCOORD0;
+            //    UNITY_FOG_COORDS(1)
+            //    float4 vertex : SV_POSITION;
+            //};
+
+            //sampler2D _MainTex;
+            //float4 _MainTex_ST;
+
+            //v2f vert (appdata v)
+            //{
+            //    v2f o;
+            //    o.vertex = UnityObjectToClipPos(v.vertex);
+            //    o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+            //    UNITY_TRANSFER_FOG(o,o.vertex);
+            //    return o;
+            //}
+
+            //fixed4 frag (v2f i) : SV_Target
+            //{
+            //    // sample the texture
+            //    fixed4 col = tex2D(_MainTex, i.uv);
+            //    // apply fog
+            //    UNITY_APPLY_FOG(i.fogCoord, col);
+            //    return col;
+            //}
+            ENDCG
+        }
+    }
+}
